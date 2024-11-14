fd0d77aa21533db734f863097ee8f23433aa7dbf
Xabiertxo <74993292+Xabietrxo@users.noreply.github.com>
2024-11-14 16:53:08 +0100

    Add files via upload
    Processes spawn correctly, but fails shrinking.

--- my_mat_batu_adm_it.c
+++ my_mat_batu_adm_it.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <mpi.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
 #include <empi.h>
+#include <stdbool.h>
+
 
 #define ROWS 5
 #define COLS 10
@@ -33,28 +38,33 @@ int main(int argc, char *argv[]) {
     int it=0;
     int itmax=ROWS*COLS;
 
-    int matrix1[ROWS*COLS];
-    int matrix2[ROWS*COLS];
+    int *matrix1 = NULL;
+    int *matrix2 = NULL;
     int result[ROWS*COLS];
 
     int *buffer = NULL;
 
-    int rank, size;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(ADM_COMM_WORLD, &rank);
-    MPI_Comm_size(ADM_COMM_WORLD, &size);
-
     MPI_Datatype intercalate_type;                                  //A type to gather intercalated
 
-    if (rank == 0) {
+    matrix1 = (int *)malloc(ROWS * COLS * sizeof(int));
+    matrix2 = (int *)malloc(ROWS * COLS * sizeof(int));
+
+    int world_rank, world_size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(ADM_COMM_WORLD, &world_rank);
+    MPI_Comm_size(ADM_COMM_WORLD, &world_size);
+
+    int dim = ROWS*COLS;
+
+    if (world_rank == 0) {
         srand(time(NULL));  // Initiate random seed
         generateRandomMatrix(matrix1);
         generateRandomMatrix(matrix2);
         buffer = (int *)malloc(ROWS * COLS * sizeof(int));
     }
 
-    MPI_Bcast(matrix1, ROWS * COLS, MPI_INT, 0, ADM_COMM_WORLD);    //Broadcast both matrix
-    MPI_Bcast(matrix2, ROWS * COLS, MPI_INT, 0, ADM_COMM_WORLD);
+    MPI_Bcast(matrix1, dim, MPI_INT, 0, ADM_COMM_WORLD);    //Broadcast both matrix
+    MPI_Bcast(matrix2, dim, MPI_INT, 0, ADM_COMM_WORLD);
 
     // get process type
     int proctype;
@@ -63,12 +73,12 @@ int main(int argc, char *argv[]) {
     // if process is native
     if (proctype == ADM_NATIVE) {
         
-        printf ("Rank(%d/%d): Process native\n", rank, size);
+        printf ("Rank(%d/%d): Process native\n", world_rank, world_size);
         
     // if process is spawned
     } else {
 
-        printf ("Rank(%d/%d): Process spawned\n", rank, size);
+        printf ("Rank(%d/%d): Process spawned\n", world_rank, world_size);
     }
     
     /* set max number of iterations */
@@ -81,7 +91,7 @@ int main(int argc, char *argv[]) {
     ADM_MonitoringService (ADM_SERVICE_START);
 
     // init last world zize
-    int last_size = size;
+    int last_world_size = world_size;
 
     // start loop
     for (; it < itmax; it++)        //All needed iterations to calculate the matrix sum
@@ -94,47 +104,52 @@ int main(int argc, char *argv[]) {
             excl_nodes_hint = 0;
             ADM_RegisterSysAttributesInt ("ADM_GLOBAL_HINT_NUM_PROCESS", &procs_hint);
             ADM_RegisterSysAttributesInt ("ADM_GLOBAL_HINT_EXCL_NODES", &excl_nodes_hint);
-            printf("Rank (%d/%d): Iteration:= %d, procs_hint=%d, excl_nodes_hint=%d\n", rank, size, it, procs_hint, excl_nodes_hint);
+            printf("Rank (%d/%d): Iteration:= %d, procs_hint=%d, excl_nodes_hint=%d\n", world_rank, world_size, it, procs_hint, excl_nodes_hint);
         } else if ( (it == 6*(itmax/10)) || (it == 8*(itmax/10)) ){
             procs_hint = -2;
             excl_nodes_hint = 0;
             ADM_RegisterSysAttributesInt ("ADM_GLOBAL_HINT_NUM_PROCESS", &procs_hint);
             ADM_RegisterSysAttributesInt ("ADM_GLOBAL_HINT_EXCL_NODES", &excl_nodes_hint);
-            printf("Rank (%d/%d): Iteration:= %d, procs_hint=%d, excl_nodes_hint=%d\n", rank, size, it, procs_hint, excl_nodes_hint);
+            printf("Rank (%d/%d): Iteration:= %d, procs_hint=%d, excl_nodes_hint=%d\n", world_rank, world_size, it, procs_hint, excl_nodes_hint);
         }
         
         // update message if new spawned processes
-        if (last_size != size) {
+        if (last_world_size != world_size) {
             
-            MPI_Type_vector(it/size, 1, size, MPI_INT, &intercalate_type);
+            MPI_Type_vector(it/world_size, 1, world_size, MPI_INT, &intercalate_type);
             MPI_Type_commit(&intercalate_type);
 
-            if (rank==0)
+            if (last_world_size<world_size)
             {
-                for (int i = 1; i < last_size; i++)
+                MPI_Bcast(matrix1, dim, MPI_INT, 0, ADM_COMM_WORLD);    //Broadcast both matrix
+                MPI_Bcast(matrix2, dim, MPI_INT, 0, ADM_COMM_WORLD);
+            }
+            if (world_rank!=0)
+            {
+                MPI_Send(&result[world_rank], 1, intercalate_type, 0, 0, ADM_COMM_WORLD);
+            }
+            if (world_rank==0)
+            {
+                for (int i = 1; i < last_world_size; i++)
                 {
                     MPI_Recv(&buffer[i], 1, intercalate_type, i, 0, ADM_COMM_WORLD, MPI_STATUS_IGNORE);
                 }
             }
 
-            if (last_size<size)
-            {
-                MPI_Bcast(matrix1, ROWS * COLS, MPI_INT, 0, ADM_COMM_WORLD);    //Broadcast both matrix
-                MPI_Bcast(matrix2, ROWS * COLS, MPI_INT, 0, ADM_COMM_WORLD);
-            }
-
-            last_size = size;
+            last_world_size = world_size;
         }
         
         /* start malelability region */
         ADM_MalleableRegion (ADM_SERVICE_START);
 
-        if (it%size == rank)        //Divide workload in processes
+        if (it%world_size == world_rank)        //Divide workload in processes
         {
             result[it] = matrix1[it] + matrix2[it];
         }
 
-        printf("Rank (%d/%d): Iteration= %d\n", rank, size, it);
+        sleep(1);
+
+        printf("Rank (%d/%d): Iteration= %d\n", world_rank, world_size, it);
 
         // update the iteration value
         ADM_RegisterSysAttributesInt ("ADM_GLOBAL_ITERATION", &it);
@@ -145,10 +160,11 @@ int main(int argc, char *argv[]) {
         
         if (status == ADM_ACTIVE) {
             // updata rank and size
-            MPI_Comm_rank(ADM_COMM_WORLD, &rank);
-            MPI_Comm_size(ADM_COMM_WORLD, &size);
+            MPI_Comm_rank(ADM_COMM_WORLD, &world_rank);
+            MPI_Comm_size(ADM_COMM_WORLD, &world_size);
         } else {
-            MPI_Send(&result[rank], 1, intercalate_type, 0, 0, ADM_COMM_WORLD);
+            
+            
             // end the process
             break;
         }
@@ -157,7 +173,7 @@ int main(int argc, char *argv[]) {
     /* ending monitoring service */
     ADM_MonitoringService (ADM_SERVICE_STOP);
 
-    printf("Rank (%d/%d): End of loop \n", rank, size);
+    printf("Rank (%d/%d): End of loop \n", world_rank, world_size);
 
 //!!!!------HAU ALDATZEKO-------!!!!------------------------(pentsatu nun jarri)------------------------------------------
 /*    if (rank!=0)
@@ -171,7 +187,9 @@ int main(int argc, char *argv[]) {
     }*/
 //!!!!------HAU ALDATZEKO-------!!!!------------------------(pentsatu nun jarri)------------------------------------------
 
-    if (rank == 0) {
+    MPI_Finalize();
+
+    if (world_rank == 0) {
         printf("Matrix 1:\n");
         printMatrix(matrix1);
 
@@ -184,6 +202,6 @@ int main(int argc, char *argv[]) {
         free(buffer);
     }
 
-    MPI_Finalize();
+    
     return 0;
 }
